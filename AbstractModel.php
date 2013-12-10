c85d0c37eeed2d880a16bb1966e010abe4dd52f0
Zachdidit <zachdidit@gmail.com>
2013-12-10 01:55:12 -0500

    Create AbstractModel.php

---
+++ AbstractModel.php
@@ -0,0 +1,21 @@
+<?php
+
+
+Class AbstractModel{
+	public function save(){
+		
+	}
+	public function load($id){
+		
+	}
+	public function delete($id){
+		
+	}
+	public function getData($key=false){
+		
+	}
+	public function setData($arr, $value=false){
+		
+	}
+}
+?>
