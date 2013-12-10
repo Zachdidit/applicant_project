2e82d97119cccf06ad6ae09a09a7f81276367e2f
Zachdidit <zachdidit@gmail.com>
2013-12-10 01:31:43 -0500

    Update contact_tests.php
    Made a minor change o the end to reset the data for the first field at the end of the test.

--- contact_tests.php
+++ contact_tests.php
@@ -1,12 +1,13 @@
 <?PHP
 require_once("AbstractModel.php");
 require_once("Contact.php");
-ini_set("display_errors", true);
+error_reporting(E_ERROR | E_WARNING);
 
 $contact = new Contact();
 
 $contact->load(1);
 print_r($contact->getData()); 
+
 // Should print:
 // id => 1,
 // name => John Doe
@@ -15,7 +16,6 @@ echo '<br/><br/>';
 echo $contact->getData('name');
 //Should print:
 // John Doe
-
 $contact->setData('name', 'John Walker')->save(); //Should run an UPDATE query
 echo '<br/><br/>';
 print_r($contact->load(1)->getData());
@@ -23,11 +23,10 @@ print_r($contact->load(1)->getData());
 // id => 1,
 // name => John Walker
 // email => john@doe.com
-
 $contact->setData(array(
-"id" => 1,
-"name" => "John Doe the 2nd",
-"email" => "john@doe2.com"
+		"id" => 1,
+		"name" => "John Doe the 2nd",
+		"email" => "john@doe2.com"
 ))->save();
 echo '<br/><br/>';
 print_r($contact->load(1)->getData());
@@ -35,15 +34,12 @@ print_r($contact->load(1)->getData());
 // id => 1,
 // name => John Doe the 2nd
 // email => john@doe2.com
-
 $newContact = new Contact();
 $newContact->setData(array(
-    "name" => "Gilbert Barber",
-    "email" => "gilbTheparrot@gmail.com"
+		"name" => "Gilbert Barber",
+		"email" => "gilbTheparrot@gmail.com"
 ));
-
 $newContact->save(); //Should run an INSERT query as there is no predefined id
-echo '<br/><br/>';
 print_r($newContact->getData());
 //Should print
 // id => ? some auto increment number,
@@ -51,3 +47,10 @@ print_r($newContact->getData());
 // email => gilbTheparrot@gmail.com
 
 $newContact->delete(); //Should delete him Gilbert Barber from the database
+
+// Added this line to reset row for testing.
+$contact->setData(array(
+		"id" => 1,
+		"name" => "John Doe",
+		"email" => "john@doe.com"
+))->save();
